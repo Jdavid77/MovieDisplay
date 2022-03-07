5e1b736de6f406b910f1dadcc02ceccf60c8a6c5
João David Nóbrega <jnobrega@neoception.com>
2022-03-07 21:52:15 +0000

    First Commit

---
+++ moviedisplay/src/main/java/com/joaodavid/moviedisplay/MoviedisplayApplication.java
@@ -0,0 +1,13 @@
+package com.joaodavid.moviedisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedisplayApplication.class, args);
+	}
+
+}
---
+++ moviedisplay/src/test/java/com/joaodavid/moviedisplay/MoviedisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodavid.moviedisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
