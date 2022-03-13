4f43d409234510c8c4a04752c7dc52108d4ac28f
João David Nóbrega <jnobrega@neoception.com>
2022-03-13 18:49:07 +0000

    Fixed imports

--- frontend/src/pages/form/index.tsx
+++ frontend/src/pages/form/index.tsx
@@ -1,6 +1,5 @@
-import { Link, useParams } from 'react-router-dom'
+
 import FormCard from '../../components/formcard';
-import { Movie } from "../../types/movie";
 
 
 export default function Form() {
